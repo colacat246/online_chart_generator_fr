047943eceebf13b23a55c83d81894ce9c01f3238
jyj <jiangyuanji@126.com>
2023-10-23 10:29:39 +0800

    优化vite打包分块

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,20 @@ const opts = {
 export default defineConfig({
   build: {
     sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
   server: {
     // https: opts,
